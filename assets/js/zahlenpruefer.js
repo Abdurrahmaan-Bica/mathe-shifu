878b99082d734d8a14b341798555b61d3a1eaa63
codelove <abdurrahmaanbica123@gmail.com>
2024-12-08 21:55:05 +0100

    Added an input range for the method that checks odd and even numbers on the zahlenpruefer.js file. Renamed the element ueber das Projekt in the navbar of the zahlenpruefer file to Ableitungs- und Integralassistent

--- assets/js/zahlenpruefer.js
+++ assets/js/zahlenpruefer.js
@@ -1,3 +1,12 @@
+
+// Diese Funktion ueberprueft , ob die Zahl gerade ist.
 function istEineGeradeZahl(eingegebeneZahl) {
-    return (eingegebeneZahl % 2 === 0) ? `Die Zahl ${eingegebeneZahl} ist gerade!` : `Die Zahl ${eingegebeneZahl} ist ungerade!`;
+
+    if (eingegebeneZahl<=0){
+        return 'Zahl muss größer als 0 sein.'
+    }
+    else{
+        return (eingegebeneZahl % 2 === 0) ? `Die Zahl ${eingegebeneZahl} ist gerade!` : `Die Zahl ${eingegebeneZahl} ist ungerade!`;
+    }
+    
 }
