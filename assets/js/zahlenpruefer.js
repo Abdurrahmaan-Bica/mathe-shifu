7c607c54561be4d94977304630cd416bcb5547f0
codelove <abdurrahmaanbica123@gmail.com>
2024-12-08 23:05:33 +0100

    Added media queries. Added same favicon for all pages. Deleted Ableitungs- und Integralrechner.

--- assets/js/zahlenpruefer.js
+++ assets/js/zahlenpruefer.js
@@ -1,12 +1,23 @@
-
 // Diese Funktion ueberprueft , ob die Zahl gerade ist.
 function istEineGeradeZahl(eingegebeneZahl) {
-
-    if (eingegebeneZahl<=0){
-        return 'Zahl muss größer als 0 sein.'
-    }
-    else{
+    if (eingegebeneZahl <= 0) {
+        return 'Zahl muss größer als 0 sein.';
+    } else {
         return (eingegebeneZahl % 2 === 0) ? `Die Zahl ${eingegebeneZahl} ist gerade!` : `Die Zahl ${eingegebeneZahl} ist ungerade!`;
     }
-    
 }
+
+$(document).ready(function() {
+    $('#checkButton').click(function() {
+        let eingegebeneZahl = $('#numberInput').val();
+        
+        if (eingegebeneZahl === '' || isNaN(eingegebeneZahl)) {
+            $('#result').text('Bitte geben Sie eine gültige Zahl ein!');
+        } else {
+            // Konvertierung zu Integer
+            eingegebeneZahl = parseInt(eingegebeneZahl);
+            let result = istEineGeradeZahl(eingegebeneZahl); 
+            $('#result').text(result);
+        }
+    });
+});
